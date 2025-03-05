7fcf16c1a874fe98b575d8d5d8e01a158f9e1036
NeerajKumar-1044 <neerajkumar.cs1044@gmail.com>
2025-03-05 22:33:57 +0530

    refactor: rename project to drawcanvas and integrate Excalidraw component

--- src/App.jsx
+++ src/App.jsx
@@ -1,13 +1,16 @@
-import { useState } from 'react';
-import Draw from './Components/Draw'
+import { useState } from 'react'
+import { Excalidraw } from "@excalidraw/excalidraw";
+
 
 function App() {
+  const [count, setCount] = useState(0)
 
   return (
     <>
-        <div className='w-full h-screen border-2 border-red-400'>
-        <Draw/>
-        </div>
+      <h1 className='text-2xl'>Excalidraw Example</h1>
+      <div className='w-full h-screen'>
+        <Excalidraw />
+      </div>
     </>
   )
 }
